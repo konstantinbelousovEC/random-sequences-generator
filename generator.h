12b500ff0cbf232670eaf42f84d2d7750d94b418
konstantinbelousovEC <kb_sd@yahoo.com>
2023-09-20 16:18:40 +0400

    refactoring of generate methods, added warnings options to complile process, changed location of print functions

--- generator.h
+++ generator.h
@@ -17,146 +17,161 @@ namespace gen {
 
     namespace sfinae {
 
-        template <typename Container>
-        constexpr auto test_push_back(typename Container::value_type) -> decltype(std::declval<Container>().push_back(std::declval<typename Container::value_type>()), std::true_type{});
+        template<typename Container>
+        constexpr auto test_push_back(typename Container::value_type) -> decltype(std::declval<Container>().push_back(
+                std::declval<typename Container::value_type>()), std::true_type{});
 
-        template <typename Container>
+        template<typename Container>
         constexpr auto test_push_back(...) -> std::false_type;
 
-        template <typename Container>
+        template<typename Container>
         constexpr bool has_push_back_method() {
             return decltype(test_push_back<Container>(typename Container::value_type{}))::value;
         }
 
-    // -----------------------------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------
 
-        template <typename Container>
-        constexpr auto test_push_front(typename Container::value_type) -> decltype(std::declval<Container>().push_front(std::declval<typename Container::value_type>()), std::true_type{});
+        template<typename Container>
+        constexpr auto test_push_front(typename Container::value_type) -> decltype(std::declval<Container>().push_front(
+                std::declval<typename Container::value_type>()), std::true_type{});
 
-        template <typename Container>
+        template<typename Container>
         constexpr auto test_push_front(...) -> std::false_type;
 
-        template <typename Container>
+        template<typename Container>
         constexpr bool has_push_front_method() {
             return decltype(test_push_front<Container>(typename Container::value_type{}))::value;
         }
 
-    // -----------------------------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------
 
-        template <typename Container>
-        constexpr auto test_insert(typename Container::value_type) -> decltype(std::declval<Container>().insert(std::declval<typename Container::value_type>()), std::true_type{});
+        template<typename Container>
+        constexpr auto test_insert(typename Container::value_type) -> decltype(std::declval<Container>().insert(
+                std::declval<typename Container::value_type>()), std::true_type{});
 
-        template <typename Container>
+        template<typename Container>
         constexpr auto test_insert(...) -> std::false_type;
 
-        template <typename Container>
+        template<typename Container>
         constexpr bool has_insert_method() {
             return decltype(test_insert<Container>(typename Container::value_type{}))::value;
         }
 
-    // -----------------------------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------
 
-        template <typename Container>
-        constexpr auto test_reserve(typename Container::size_type) -> decltype(std::declval<Container>().reserve(std::declval<typename Container::size_type>()), std::true_type{});
+        template<typename Container>
+        constexpr auto test_reserve(typename Container::size_type) -> decltype(std::declval<Container>().reserve(
+                std::declval<typename Container::size_type>()), std::true_type{});
 
-        template <typename Container>
+        template<typename Container>
         constexpr auto test_reserve(...) -> std::false_type;
 
-        template <typename Container>
+        template<typename Container>
         constexpr bool has_reserve_method() {
             return decltype(test_reserve<Container>(typename Container::size_type{}))::value;
         }
 
-    // -----------------------------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------
 
-        template <typename T>
-        struct is_std_seq_container : std::false_type {};
+        template<typename T>
+        struct is_std_seq_container : std::false_type {
+        };
 
-        template <typename T>
-        struct is_std_seq_container<std::vector<T>> : std::true_type {};
+        template<typename T>
+        struct is_std_seq_container<std::vector<T>> : std::true_type {
+        };
 
-        template <typename T>
-        struct is_std_seq_container<std::deque<T>> : std::true_type {};
+        template<typename T>
+        struct is_std_seq_container<std::deque<T>> : std::true_type {
+        };
 
-        template <typename T>
-        struct is_std_seq_container<std::list<T>> : std::true_type {};
+        template<typename T>
+        struct is_std_seq_container<std::list<T>> : std::true_type {
+        };
 
-        template <typename T>
-        struct is_std_seq_container<std::forward_list<T>> : std::true_type {};
+        template<typename T>
+        struct is_std_seq_container<std::forward_list<T>> : std::true_type {
+        };
 
-        template <typename T>
+        template<typename T>
         constexpr bool is_std_seq_container_v = is_std_seq_container<T>::value;
 
-    // -----------------------------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------
 
-        template <typename T>
-        struct is_std_associative_val_only_container : std::false_type {};
+        template<typename T>
+        struct is_std_associative_val_only_container : std::false_type {
+        };
 
-        template <typename T>
-        struct is_std_associative_val_only_container<std::set<T>> : std::true_type {};
+        template<typename T>
+        struct is_std_associative_val_only_container<std::set<T>> : std::true_type {
+        };
 
-        template <typename T>
-        struct is_std_associative_val_only_container<std::unordered_set<T>> : std::true_type {};
+        template<typename T>
+        struct is_std_associative_val_only_container<std::unordered_set<T>> : std::true_type {
+        };
 
-        template <typename T>
+        template<typename T>
         constexpr bool is_std_associative_val_only_container_v = is_std_associative_val_only_container<T>::value;
 
-    // -----------------------------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------
 
-        template <typename T>
-        struct is_std_associative_key_val_container : std::false_type {};
+        template<typename T>
+        struct is_std_associative_key_val_container : std::false_type {
+        };
 
-        template <typename T, typename U>
-        struct is_std_associative_key_val_container<std::map<T, U>> : std::true_type {};
+        template<typename T, typename U>
+        struct is_std_associative_key_val_container<std::map<T, U>> : std::true_type {
+        };
 
-        template <typename T, typename U>
-        struct is_std_associative_key_val_container<std::unordered_map<T, U>> : std::true_type {};
+        template<typename T, typename U>
+        struct is_std_associative_key_val_container<std::unordered_map<T, U>> : std::true_type {
+        };
 
-        template <typename T>
+        template<typename T>
         constexpr bool is_std_associative_key_val_container_v = is_std_associative_key_val_container<T>::value;
 
-    } // namespace sfinae
+    }  // namespace sfinae
 
     template<typename T> using enable_if_std_seq_container = std::enable_if_t<sfinae::is_std_seq_container_v<T>, T>;
     template<typename T> using enable_if_std_actv_val_container = std::enable_if_t<sfinae::is_std_associative_val_only_container_v<T>, T>;
     template<typename T> using enable_if_std_actv_key_val_container = std::enable_if_t<sfinae::is_std_associative_key_val_container_v<T>, T>;
 
     template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
-    struct ArithmeticValueGenerator {
-    private:
+    struct ArithmeticValueGenerator final {
+     private:
         T min_ = std::numeric_limits<T>::min();
         T max_ = std::numeric_limits<T>::max();
 
-    public:
+     public:
         ArithmeticValueGenerator() = default;
+
         ArithmeticValueGenerator(T min, T max) : min_(min), max_(max) {}
 
         template<typename BitGen>
-        T operator() (BitGen& gen) {
+        T operator()(BitGen& gen) {
             if constexpr (std::is_integral<T>::value) {
-                return std::uniform_int_distribution<T> {min_, max_}(gen);
+                return std::uniform_int_distribution<T>{min_, max_}(gen);
             } else {
-                return std::uniform_real_distribution<T> {min_, max_}(gen);
+                return std::uniform_real_distribution<T>{min_, max_}(gen);
             }
         }
+    };
 
-    }; // ArithmeticValueGenerator
-
-    struct StringValueGenerator {
-    private:
+    struct StringValueGenerator final {
+     private:
         using str_sz = std::string::size_type;
 
-    private:
+     private:
         str_sz min_sz_;
         str_sz max_sz_;
         std::string char_coll_;
 
-    public:
+     public:
         StringValueGenerator(str_sz min_size, str_sz max_size, std::string char_coll)
-        : min_sz_(min_size), max_sz_(max_size), char_coll_(std::move(char_coll )) {}
+                : min_sz_(min_size), max_sz_(max_size), char_coll_(std::move(char_coll)) {}
 
         template<typename BitGen>
-        std::string operator() (BitGen& gen) {
+        std::string operator()(BitGen& gen) {
             std::uniform_int_distribution<str_sz> str_size_dist(min_sz_, max_sz_);
             str_sz str_size = str_size_dist(gen);
 
@@ -165,100 +180,110 @@ namespace gen {
 
             std::uniform_int_distribution<str_sz> dist(0, char_coll_.size() - 1);
 
-            for (int i = 0; i < str_size; ++i) {
+            for (str_sz i = 0; i < str_size; ++i) {
                 rand_str += char_coll_[dist(gen)];
             }
 
             return rand_str;
         }
+    };
 
-    }; // StringValueGenerator
-
-    class Generator {
-    public:
+    class Generator final {
+     public:
         template<typename Container> using size_type = typename Container::size_type;
         template<typename Container> using value_type = typename Container::value_type;
         template<typename AssociativeContainer> using key_type = typename AssociativeContainer::key_type;
         template<typename AssociativeContainer> using mapped_type = typename AssociativeContainer::mapped_type;
 
-    private:
+     private:
         std::random_device rd_;
         std::mt19937 gen_;
 
-    public:
+     public:
         Generator() : rd_(), gen_(rd_()) {}
 
-        template<typename SequenceContainer, typename ValueGenerator = ArithmeticValueGenerator<value_type<SequenceContainer>>>
-        enable_if_std_seq_container<SequenceContainer> generate(size_type<SequenceContainer> size,
-                                                                ValueGenerator val_gen = ArithmeticValueGenerator<value_type<SequenceContainer>>{});
+        template<typename Container,
+                 typename ValueGenerator = ArithmeticValueGenerator<value_type<Container>>,
+                 enable_if_std_seq_container<Container>* = nullptr>
+        Container generate(size_type<Container> size,
+                           ValueGenerator val_gen = {}) {
+            Container container;
+            if constexpr (sfinae::has_reserve_method<Container>()) container.reserve(size);
 
-        template<typename SequenceContainer, typename ValueGenerator = ArithmeticValueGenerator<value_type<SequenceContainer>>>
-        enable_if_std_actv_val_container<SequenceContainer> generate(size_type<SequenceContainer> size,
-                                                                     ValueGenerator val_gen = ArithmeticValueGenerator<value_type<SequenceContainer>>{});
-
-        template<typename SequenceContainer, typename KeyGenerator = ArithmeticValueGenerator<key_type<SequenceContainer>>, typename ValueGenerator = ArithmeticValueGenerator<mapped_type<SequenceContainer>>>
-        enable_if_std_actv_key_val_container<SequenceContainer> generate(size_type<SequenceContainer> size,
-                                                                         KeyGenerator key_gen = ArithmeticValueGenerator<value_type<SequenceContainer>>{},
-                                                                         ValueGenerator val_gen = ArithmeticValueGenerator<value_type<SequenceContainer>>{});
-
-    }; // Generator
-
-    template<typename SequenceContainer, typename ValueGenerator>
-    enable_if_std_seq_container<SequenceContainer> Generator::generate(size_type<SequenceContainer> size,
-                                                                       ValueGenerator val_gen)
-    {
-        SequenceContainer container;
-        if constexpr (sfinae::has_reserve_method<SequenceContainer>()) container.reserve(size);
-
-        for (size_type<SequenceContainer> i = 0; i < size; ++i) {
-            if constexpr (sfinae::has_push_back_method<SequenceContainer>()) {
-                container.push_back(val_gen(gen_));
-            } else if constexpr (sfinae::has_push_front_method<SequenceContainer>()) {
-                container.push_front(val_gen(gen_));
+            for (size_type<Container> i = 0; i < size; ++i) {
+                if constexpr (sfinae::has_push_back_method<Container>()) {
+                    container.push_back(val_gen(gen_));
+                } else if constexpr (sfinae::has_push_front_method<Container>()) {
+                    container.push_front(val_gen(gen_));
+                }
             }
+
+            return container;
         }
 
-        return container;
-    }
+        template<typename Container,
+                 typename ValueGenerator = ArithmeticValueGenerator<value_type<Container>>,
+                 enable_if_std_actv_val_container<Container>* = nullptr>
+        Container generate(size_type<Container> size,
+                           ValueGenerator val_gen = {}) {
+            // the size of container must be more or equal to all possible unique values in range [val_gen.min, val_gen.max]
+            // if this condition is false - UB or infinite cycle may happen
+            // todo: add check for this condition - it will throw an exception then
 
-    template<typename SequenceContainer, typename ValueGenerator>
-    enable_if_std_actv_val_container<SequenceContainer> Generator::generate(size_type<SequenceContainer> size,
-                                                                            ValueGenerator val_gen)
-    {
-        // the size of container must be more or equal to all possible unique values in range [val_gen.min, val_gen.max]
-        // if this condition is false - UB or infinite cycle may happen
-        // todo: add check for this condition - it will throw an exception then
+            Container container;
 
-        SequenceContainer container;
-
-        for (size_type<SequenceContainer> i = 0; i < size; ++i) {
-            auto insert_info = container.insert(val_gen(gen_));
-            while (!insert_info.second) {
-                insert_info = container.insert(val_gen(gen_));
+            for (size_type<Container> i = 0; i < size; ++i) {
+                auto insert_info = container.insert(val_gen(gen_));
+                while (!insert_info.second) {
+                    insert_info = container.insert(val_gen(gen_));
+                }
             }
+
+            return container;
         }
 
-        return container;
-    }
+        template<typename Container,
+                 typename KeyGenerator = ArithmeticValueGenerator<key_type<Container>>,
+                 typename ValueGenerator = ArithmeticValueGenerator<mapped_type<Container>>,
+                 enable_if_std_actv_key_val_container<Container>* = nullptr>
+        Container generate(size_type<Container> size,
+                           KeyGenerator key_gen = {},
+                           ValueGenerator val_gen = {}) {
+            // the size of container must be more or equal to all possible unique values in range [val_gen.min, val_gen.max]
+            // if this condition is false - UB or infinite cycle may happen
+            // todo: add check for this condition - it will throw an exception then
 
-    template<typename SequenceContainer, typename KeyGenerator, typename ValueGenerator>
-    enable_if_std_actv_key_val_container<SequenceContainer>
-    Generator::generate(size_type<SequenceContainer> size, KeyGenerator key_gen, ValueGenerator val_gen)
-    {
-        // the size of container must be more or equal to all possible unique values in range [val_gen.min, val_gen.max]
-        // if this condition is false - UB or infinite cycle may happen
-        // todo: add check for this condition - it will throw an exception then
+            Container container;
 
-        SequenceContainer container;
-
-        for (size_type<SequenceContainer> i = 0; i < size; ++i) {
-            auto insert_info = container.insert({key_gen(gen_), val_gen(gen_)});
-            while (!insert_info.second) {
-                insert_info = container.insert({key_gen(gen_), val_gen(gen_)});
+            for (size_type<Container> i = 0; i < size; ++i) {
+                auto insert_info = container.insert({key_gen(gen_), val_gen(gen_)});
+                while (!insert_info.second) {
+                    insert_info = container.insert({key_gen(gen_), val_gen(gen_)});
+                }
             }
+
+            return container;
+        }
+    };
+
+    namespace print {
+
+        template<typename Iterator>
+        void print(Iterator start, Iterator finish, std::ostream& out) {
+            for (auto iterator = start; iterator != finish; ++iterator) {
+                out << *iterator << " ";
+            }
+            out << std::endl;
         }
 
-        return container;
-    }
+        template<typename Iterator>
+        void print_pair(Iterator start, Iterator finish, std::ostream& out) {
+            for (auto iterator = start; iterator != finish; ++iterator) {
+                out << iterator->first << " : " << iterator->second << " | ";
+            }
+            out << std::endl;
+        }
 
-} // namespace gen
+    }  // namespace print
+
+}  // namespace gen
--- main.cpp
+++ main.cpp
@@ -3,22 +3,6 @@
 
 #include "generator.h"
 
-template<typename Iterator>
-void print(Iterator start, Iterator finish, std::ostream& out) {
-    for (auto curr_iter = start; curr_iter != finish; ++curr_iter) {
-        out << *curr_iter << " ";
-    }
-    out << std::endl;
-}
-
-template<typename Iterator>
-void print_pair(Iterator start, Iterator finish, std::ostream& out) {
-    for (auto curr_iter = start; curr_iter != finish; ++curr_iter) {
-        out << curr_iter->first << " : " << curr_iter->second << " | ";
-    }
-    out << std::endl;
-}
-
 class X {
 private:
     int x_{};
@@ -66,47 +50,47 @@ int main() {
     // -----------------------------------------------------------------------------------------------------------------
 
     auto rand_v_1 = generator.generate<std::vector<int>>(10, {10, 24});
-    print(rand_v_1.begin(), rand_v_1.end(), std::cout);
+    gen::print::print(rand_v_1.begin(), rand_v_1.end(), std::cout);
 
     auto rand_d_1 = generator.generate<std::deque<int>>(10, {25, 39});
-    print(rand_d_1.begin(), rand_d_1.end(), std::cout);
+    gen::print::print(rand_d_1.begin(), rand_d_1.end(), std::cout);
 
     auto rand_l_1 = generator.generate<std::list<int>>(10, {40, 54});
-    print(rand_l_1.begin(), rand_l_1.end(), std::cout);
+    gen::print::print(rand_l_1.begin(), rand_l_1.end(), std::cout);
 
     auto rand_fl_1 = generator.generate<std::forward_list<int>>(10, {55, 69});
-    print(rand_fl_1.begin(), rand_fl_1.end(), std::cout);
+    gen::print::print(rand_fl_1.begin(), rand_fl_1.end(), std::cout);
 
     // -----------------------------------------------------------------------------------------------------------------
 
     auto rand_s_1 = generator.generate<std::set<int>>(10, {0, 9});
-    print(rand_s_1.begin(), rand_s_1.end(), std::cout);
+    gen::print::print(rand_s_1.begin(), rand_s_1.end(), std::cout);
 
     auto rand_us_1 = generator.generate<std::unordered_set<int>>(10, {10, 19});
-    print(rand_us_1.begin(), rand_us_1.end(), std::cout);
+    gen::print::print(rand_us_1.begin(), rand_us_1.end(), std::cout);
 
     // -----------------------------------------------------------------------------------------------------------------
 
     auto rand_m_1 = generator.generate<std::map<int, double>>(5, {0, 9}, {0.0, 1.0});
-    print_pair(rand_m_1.begin(), rand_m_1.end(), std::cout);
+    gen::print::print_pair(rand_m_1.begin(), rand_m_1.end(), std::cout);
 
     auto rand_um_1 = generator.generate<std::unordered_map<int, double>>(5, {10, 30}, {10.0, 11.0});
-    print_pair(rand_um_1.begin(), rand_um_1.end(), std::cout);
+    gen::print::print_pair(rand_um_1.begin(), rand_um_1.end(), std::cout);
 
     // -----------------------------------------------------------------------------------------------------------------
 
     gen::StringValueGenerator str_gen_1{4, 5, "abc123"};
     auto rand_v_2 = generator.generate<std::vector<std::string>>(10, str_gen_1);
-    print(rand_v_2.begin(), rand_v_2.end(), std::cout);
+    gen::print::print(rand_v_2.begin(), rand_v_2.end(), std::cout);
 
     gen::StringValueGenerator str_gen_2{2, 2, "ab"};
     auto rand_s_2 = generator.generate<std::set<std::string>>(4, str_gen_2);
-    print(rand_s_2.begin(), rand_s_2.end(), std::cout);
+    gen::print::print(rand_s_2.begin(), rand_s_2.end(), std::cout);
 
     gen::StringValueGenerator str_gen_3{3, 3, "klx"};
     gen::StringValueGenerator str_gen_4{5, 5, "aAbBcCdDeE12345"};
     auto rand_m_2 = generator.generate<std::map<std::string, std::string>>(5, str_gen_3, str_gen_4);
-    print_pair(rand_m_2.begin(), rand_m_2.end(), std::cout);
+    gen::print::print_pair(rand_m_2.begin(), rand_m_2.end(), std::cout);
 
     // -----------------------------------------------------------------------------------------------------------------
 
